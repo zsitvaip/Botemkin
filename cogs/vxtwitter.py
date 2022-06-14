c3e073905f1436a073d284638c9a89379b25b5c7
pjotron <pervuj.pjotr@gmail.com>
2022-06-15 00:41:47 +0200

    Fix permission check in vxtwitter cog on_raw_reaction_add

--- cogs/vxtwitter.py
+++ cogs/vxtwitter.py
@@ -46,8 +46,9 @@ class Vxtwitter(commands.Cog):
             # temporarily couldn't fetch ref, user should retry later
             return
         try:
-            if ref.author == payload.user_id or payload.member.permissions_in(channel).manage_messages:
-                await ref.edit(suppress=False)
+            if ref.author != payload.user_id and payload.member.permissions_in(channel).manage_messages:
+                return
+            await ref.edit(suppress=False)
         except AttributeError:
             # if ref got deleted we get discord.DeletedReferencedMessage which has no author field
             pass
